049c05242dfc3d1c8193ef0522a70bf2ebcd25f6
sureshbabuvikram <sureshbabuvikram@gmail.com>
2023-11-07 17:17:21 +0530

    path

--- backend-app/index.js
+++ backend-app/index.js
@@ -6,6 +6,7 @@ import connectDB from './dbConfig.js'
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
