fcf405bb7a383367b725c84c5eeacdda8676868d
sureshbabuvikram <sureshbabuvikram@gmail.com>
2023-11-07 16:01:26 +0530

    url

--- backend-app/index.js
+++ backend-app/index.js
@@ -3,9 +3,14 @@ import cors from 'cors'
 import multer from 'multer'
 import Image from './image.schema.js'
 import connectDB from './dbConfig.js'
-import path from 'path'
 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 console.log('__dirname:', __dirname);
+
 
 // const destinationPath = path.join(__dirname, '../frontend-app/src/images')
 const storage = multer.diskStorage({
